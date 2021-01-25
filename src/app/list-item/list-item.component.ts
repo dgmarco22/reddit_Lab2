141abaaa4ea477a4ecab611c4dc8a580c0e9fa9a
DebbieG <splurge22@yahoo.com>
2021-01-24 20:01:03 -0500

    finalcopy

--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-item',
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-item.component.css']
 })
 export class ListItemComponent implements OnInit {
-    post: any;
-  constructor() { }
+    @Input() post: any; //recognizes [post]="post" need to import input above to use
+  
+    constructor() { }
 
   ngOnInit(): void {
   }
